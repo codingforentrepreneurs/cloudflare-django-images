ba175e4a9a58f72368195058671c5222b91b1cc1
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-02 16:02:11 -0600

    3 - Create Image and BlogPost Models

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from images.models import Image
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Image, null=True, blank=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
--- src/images/models.py
+++ src/images/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    cloudflare_id = models.CharField(max_length=200)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
