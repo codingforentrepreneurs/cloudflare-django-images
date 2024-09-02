613638fbea646a4424d2fdfefb178f7554da9a2b
CodingForEntrepreneurs <hello@teamcfe.com>
2024-09-02 16:38:56 -0600

    6 - Register the Blog Model in the Django Admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BlogPost
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    fields = ['title', 'content', 'author', 'timestamp', 'updated']
+    readonly_fields = ['timestamp', 'updated']
+    model = BlogPost
+
+
+# admin.site.register(BlogPost, BlogPostAdmin)
